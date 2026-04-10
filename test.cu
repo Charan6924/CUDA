73c806f3b2b8347eb13106d0ef431f56553dfba5
Charan6924 <altmrcharbot@gmail.com>
2026-04-10 15:49:34 -0400

    addition kernel optimized with prefetching and multiple blocks/threads

--- test.cu
+++ test.cu
@@ -1,39 +1,39 @@
 #include <iostream>
 #include <math.h>
 
-__global__
-void add(int n, float *sum, float *x, float *y)
+__global__ void add(int n, float *sum, float *x, float *y)
 {
-  for (int i = 0; i < n; i++)
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = index; i < n; i+=stride)
     sum[i] = x[i] + y[i];
 }
 
-int main(void)
-{
-  int N = 1<<20;
-
+int main(){
+  int N {1<<20};
   float *x, *y, *sum;
-  cudaMallocManaged(&x,   N*sizeof(float));
-  cudaMallocManaged(&y,   N*sizeof(float));
-  cudaMallocManaged(&sum, N*sizeof(float));  
+  cudaMallocManaged(&x, N*sizeof(float));
+  cudaMallocManaged(&y, N*sizeof(float));
+  cudaMallocManaged(&sum, N*sizeof(float));
 
+  
   for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
-  }
+   x[i] = 1.0f;
+   y[i] = 2.0f;
+ }
 
-  add<<<1, 1>>>(N, sum, x, y);
-  cudaDeviceSynchronize();                
+ int blockSize = 256;
+ int numBlocks = (N + blockSize - 1) / blockSize;
+ cudaMemPrefetchAsync(x, N*sizeof(float), 0, 0);
+ cudaMemPrefetchAsync(y, N*sizeof(float), 0, 0);
+ add<<<numBlocks, blockSize>>>(N, sum, x, y);
+ cudaDeviceSynchronize(); 
+ float maxError = 0.0f;
+ for (int i = 0; i < N; i++)
+   maxError = fmax(maxError, fabs(sum[i]-3.0f));
+ std::cout << "Max error: " << maxError << std::endl;
+ cudaFree(x);
+ cudaFree(y);
+ cudaFree(sum);
 
-  float maxError = 0.0f;
-  for (int i = 0; i < N; i++)
-    maxError = fmax(maxError, fabs(sum[i] - 3.0f));  
-
-  std::cout << "Max error: " << maxError << std::endl;
-
-  cudaFree(x);
-  cudaFree(y);
-  cudaFree(sum);   
-
-  return 0;
 }
