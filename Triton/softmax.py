123d1dde1d3d9c7a51f0a21a034821769f7066ed
Charan6924 <altmrcharbot@gmail.com>
2026-06-29 06:09:01 -0400

    done writing softmax

--- Triton/softmax.py
+++ Triton/softmax.py
@@ -2,7 +2,51 @@ import torch
 import triton.language as tl
 import triton
 
+def naive_softmax(x:torch.Tensor):
+    M,N  = x.shape
+    x_max = x.max(dim=1)[0]
+    x = x - x_max[:, None]
 
+    numerator = torch.exp(x)
+    denominator = numerator.sum(dim=1)
+    y = numerator / denominator[:, None]
+
+    return y
+
+def triton_softmax(x:torch.Tensor):
+    y = torch.empty_like(x)
+
+    M,N  = x.shape
+    block_size = triton.next_power_of_2(N)
+    num_blocks = M
+
+    triton_softmax_kernel[(M,)](
+        x_ptr=x, y_ptr=y,
+        x_row_stride=x.stride(0), y_row_stride=y.stride(0),
+        num_cols=N, BLOCK_SIZE=block_size
+    )
+
+    return y
+
+@triton.jit
+def triton_softmax_kernel(x_ptr,y_ptr,x_row_stride,y_row_stride,num_cols,BLOCK_SIZE:tl.constexpr):
+    assert num_cols <= BLOCK_SIZE
+
+    row_idx = tl.program_id(0)
+    col_offsets = tl.arange(0, BLOCK_SIZE)
+
+    x_start_ptr = x_ptr + row_idx * x_row_stride
+    x_ptrs = x_start_ptr + col_offsets
+    x_row = tl.load(x_ptrs, mask = col_offsets < num_cols)
+
+    x_row = x_row - tl.max(x_row, axis=0)
+    numerator = tl.exp(x_row)
+    denominator = tl.sum(numerator,axis=0)
+    y_row = numerator/denominator
+
+    y_start_ptr = y_ptr + row_idx * y_row_stride
+    y_ptrs = y_start_ptr + col_offsets
+    tl.store(y_ptrs, y_row, mask=col_offsets < num_cols)
 
 
 def output_ptx(name: str, kernel):
