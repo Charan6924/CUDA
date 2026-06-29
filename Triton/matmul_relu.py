f1cefae07c5919d2b85c397a3e25631fcefdce99
Charan6924 <altmrcharbot@gmail.com>
2026-06-29 06:56:02 -0400

    matmul_relu kernel

--- Triton/matmul_relu.py
+++ Triton/matmul_relu.py
@@ -5,6 +5,58 @@ import triton.language as tl
 def naive_matmul_relu(x:torch.Tensor,y:torch.Tensor):
     return torch.nn.functional.relu(x@y)
 
+def triton_matmul_relu(a:torch.Tensor, b:torch.Tensor):
+    assert a.is_cuda and b.is_cuda
+    assert a.is_contiguous() and b.is_contiguous()
+    assert a.shape[1] == b.shape[0]
 
+    M,K = a.shape
+    K,N = b.shape
 
+    c = torch.empty((M, N), device=a.device)
+    BLOCK_M, BLOCK_N, BLOCK_K = 64, 64, 32
+    grid = (triton.cdiv(M, BLOCK_M), triton.cdiv(N, BLOCK_N))
 
+    matmul_relu_kernel[grid](
+        a, b, c,
+        M, N, K,
+        a.stride(0), a.stride(1),
+        b.stride(0), b.stride(1),
+        c.stride(0), c.stride(1),
+        BLOCK_M, BLOCK_N, BLOCK_K,
+    )
+
+    return c
+
+@triton.jit
+def matmul_relu_kernel(
+    a_ptr, b_ptr, c_ptr,
+    M, N, K,
+    stride_am, stride_ak,
+    stride_bk, stride_bn,
+    stride_cm, stride_cn,
+    BLOCK_M: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+    BLOCK_K: tl.constexpr,):
+
+    pid_m = tl.program_id(0)
+    pid_n = tl.program_id(1)
+
+    indices_m = pid_m * BLOCK_M + tl.arange(0,BLOCK_M)
+    indices_n = pid_n * BLOCK_N + tl.arange(0,BLOCK_N)
+    indices_k = tl.arange(0, BLOCK_K)
+
+    a_ptrs = a_ptr + indices_m[:,None] * stride_am + indices_k[None, :] * stride_ak
+    b_ptrs = b_ptr + indices_k[:, None] * stride_bk + indices_n[None, :] * stride_bn
+    acc = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
+
+    for k in range(0,K,BLOCK_K):
+        a = tl.load(a_ptrs, mask=(indices_m[:, None] < M) & (indices_k[None, :] + k < K), other=0.0)
+        b = tl.load(b_ptrs, mask=(indices_k[:, None] + k < K) & (indices_n[None, :] < N), other=0.0)
+        acc += tl.dot(a,b)
+        a_ptrs += BLOCK_K * stride_ak
+        b_ptrs += BLOCK_K * stride_bk
+    
+    acc = tl.maximum(acc, 0.0)
+    c_ptrs = c_ptr + indices_m[:, None] * stride_cm + indices_n[None, :] * stride_cn
+    tl.store(c_ptrs, acc, mask=(indices_m[:, None] < M) & (indices_n[None, :] < N))
